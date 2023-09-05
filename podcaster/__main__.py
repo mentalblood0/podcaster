1db7f38e1557a4098a1bf66087b5abd6829027d2
mentalblood <neceporenkostepan@gmail.com>
2023-09-05 15:39:48 +0000

    fix

--- podcaster/__main__.py
+++ podcaster/__main__.py
@@ -57,35 +57,7 @@ def _upload(
 @click.option('--telegram',   required = True,  type = str,                                                           help = 'Telegram chat id')
 @click.option('--cache',      required = True,  type = pathlib.Path,                                                  help = 'Path to cache file')
 @click.option('--bitrate',    required = False, type = yoop.Audio.Bitrate,    default = yoop.Audio.Bitrate(80),       help = 'Resulting audio bitrate')
-@click.option('--samplerate',	print('playlist', playlist.title)
-
-	if playlist in cache:
-		return
-
-	for e in playlist[::-1]:
-		match e:
-			case yoop.Playlist():
-				_upload(e, bot, cache, bitrate, samplerate, channels)
-			case yoop.Video():
-				if e.available and e not in cache:
-					print('video', e.title.simple)
-					bot.load(
-						e.audio(
-							yoop.Audio.Bitrate(90)
-						).converted(
-							bitrate    = bitrate,
-							samplerate = samplerate,
-							format     = yoop.Audio.Format.MP3,
-							channels   = channels
-						).tagged(
-							title  = e.title.simple,
-							album  = playlist.title,
-							artist = e.uploader
-						).covered(
-							playlist.uploader.avatar.resized(150)
-						)
-					)
-				cache.add(e) required = False, type = yoop.Audio.Samplerate, default = yoop.Audio.Samplerate(32000), help = 'Resulting audio samplerate')
+@click.option('--samplerate', required = False, type = yoop.Audio.Samplerate, default = yoop.Audio.Samplerate(32000), help = 'Resulting audio samplerate')
 @click.option('--channels',   required = False, type = click.Choice([c.name for c in yoop.Audio.Channels]),   default = yoop.Audio.Channels.mono,     help = 'Resulting audio channels')
 def upload(
 	url           : yoop.Url,
