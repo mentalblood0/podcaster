ba2fa1375df20dd4fc4bcb49808e257dfa4a04b9
Нечепоренко Степан <neceporenkostepan@gmail.com>
2024-07-24 13:09:20 +0300

    human-readable enums representations

--- podcaster/enums.py
+++ podcaster/enums.py
@@ -6,8 +6,14 @@ class ConvertMode(Enum):
     NEVER = "never"
     AUTO = "auto"
 
+    def __str__(self):
+        return self.value
+
 
 class OrderMode(Enum):
     NEW_FIRST = "new_first"
     OLD_FIRST = "old_first"
     AUTO = "auto"
+
+    def __str__(self):
+        return self.value
